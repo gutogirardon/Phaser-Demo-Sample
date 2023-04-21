b6c0aa7e805e3f53827778a25823392782b1e2e2
Guto Girardon <ggirardon@gmail.com>
2023-04-21 12:45:10 -0300

    feature: new players was a little blueORred box

--- public/js/game.js
+++ public/js/game.js
@@ -1,26 +1,52 @@
 var config = {
-    type: Phaser.AUTO, //usa webgl se tiver disponivel, se nao, usa canvas
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        debug: false,
-        gravity: { y: 0 }
-      }
+  type: Phaser.AUTO, //usa webgl se tiver disponivel, se nao, usa canvas
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+      gravity: { y: 0 },
     },
-    scene: {
-      preload: preload,
-      create: create,
-      update: update
-    } 
-  };
-  var game = new Phaser.Game(config);
-  function preload() {}
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
+var game = new Phaser.Game(config);
 
-  function create() {
-    this.socket = io();
+function preload() {
+  this.load.image("ship", "assets/spaceship.png"); //carrega a imagem da nave
+}
+
+function create() {
+  var self = this;
+  this.socket = io();
+  this.socket.on("currentPlayers", function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id]);
+      }
+    });
+  });
+}
+
+function update() {}
+
+function addPlayer(self, playerInfo) {
+  self.ship = self.physics.add //vai usar a fisica arcade da llib
+    .image(playerInfo.x, playerInfo.y, "ship") //aqui vai usar a posicao x, y criada pelo servidor
+    .setOrigin(0.5, 0.5) //seta a imagem no meio e nao no canto superior esquerdo do objeto
+    .setDisplaySize(53, 40); // tamanho da nave
+  if (playerInfo.team === "blue") {
+    self.ship.setTint(0x0000ff);
+  } else {
+    self.ship.setTint(0xff0000);
   }
-  
-  function update() {}
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
+}
